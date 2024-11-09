5051cf6e89f068471db046029a03481e10a55ebd
Polina <polinscik@gmail.com>
2024-11-09 23:58:31 +0300

    publish to gh-pages

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from "react";
 import App from "./App.jsx";
 import {createRoot} from "react-dom/client";
-import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import {createHashRouter, RouterProvider} from "react-router-dom";
 import ErrorPage from "./pages/ErrorPage/ErrorPage.jsx";
 import FormPage from "./pages/FormPage/FormPage.jsx";
 import MainPage from "./pages/MainPage/MainPage.jsx";
 import PaymentPage from "./pages/PaymentPage/PaymentPage.jsx";
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: "/",
     element: <App></App>,
